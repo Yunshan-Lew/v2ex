f956cd8e812370b1568132266a8d75279c777f21
luzhipeng <alexanderde1591@gmail.com>
2018-09-14 15:15:55 +0800

    modify

--- utils/web_api.js
+++ utils/web_api.js
@@ -1,35 +1,24 @@
 'use strict';
 var HOST_URI = 'https://www.v2ex.com/api/';
 
-// 获取节点
 // 所有的节点
 var ALL_NODE = 'nodes/all.json';
-// 获取节点信息 
-// 节点id :id 节点名 :name
+// 获取节点信息 :id | :name
 var NODE_INFO = 'nodes/show.json';
-
-// 获取主题
 // 取最新的主题
 var LATEST_TOPIC = 'topics/latest.json';
 // 获取热议主题
 var HOT_TOPIC = 'topics/hot.json';
 // 获取主题信息  :id | (:username | :node_id | :node_name)
 var GET_TOPICS = 'topics/show.json';
-
-
-// 获取回复 :topic_id (:page , :page_size)?
+// 获取回复 :topic_id
 var GET_REPLIES = 'replies/show.json';
-
-
-// 获取用户信息
-var GET_USERINFO = 'members/show.json';
 
 function _obj2uri(obj){
 	return Object.keys(obj).map(function(k) {
 		return encodeURIComponent(k) + "=" + encodeURIComponent(obj[k]);
 	}).join('&');
 }
-
 
 function _getAllNode(){
 	return HOST_URI+ALL_NODE;
